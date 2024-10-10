d31be1bf1d5bdd7f39a73d60f6d68b4d40086e8b
Alexander <alexanderduli21@gmail.com>
2024-10-10 14:09:15 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: MyApp(),
   ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[200],
       appBar: AppBar(
         backgroundColor: Colors.blue[300],
-        leading: const Icon(Icons.people_outlined),
-        title: const Center(
+        leading: Icon(Icons.people_outlined),
+        title: Center(
           child: Text(
             "PEMROGRAMAN MOBILE",
             style: TextStyle(color: Colors.white),
-            textAlign: TextAlign.center,
           ),
         ),
       ),
@@ -29,50 +26,62 @@ class MyApp extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           // Menambahkan tulisan "Biodata" di atas foto
-          const Text(
-            "BIODATA MAHASISWA",
+          Text(
+            "BIODATA",
             style: TextStyle(
-              fontSize: 24.0, // Ukuran font yang lebih besar
+              fontSize: 20.0, // Ukuran font yang lebih besar
               fontWeight: FontWeight.bold, // Membuat teks menjadi bold
               color: Colors.black,
             ),
             textAlign: TextAlign.center,
           ),
-          const SizedBox(height: 16.0), // Memberikan jarak antara teks dan gambar
+          SizedBox(height: 16.0), // Memberikan jarak antara teks dan gambar
           Image.asset(
             "img/alexander.jpg",
             width: 200.0,
             height: 200.0,
           ),
-          const Text(
+          Text(
             "NAMA : ALEXANDER SABON DULI",
             style: TextStyle(
-              fontSize: 20.0,
+              fontSize: 15.0,
               color: Colors.black,
             ),
             textAlign: TextAlign.center,
           ),
-          const Text(
+          Text(
             "NIM : 20220801398",
             style: TextStyle(
-              fontSize: 20.0,
+              fontSize: 15.0,
               color: Colors.black,
             ),
             textAlign: TextAlign.center,
           ),
-          const Text(
+          Text(
             "FAKULTAS : ILMU KOMPUTER",
             style: TextStyle(
-              fontSize: 20.0,
+              fontSize: 15.0,
               color: Colors.black,
             ),
             textAlign: TextAlign.center,
           ),
-          const Text(
+          Text(
             "JURUSAN : TEKNIK INFORMATIKA",
+            style: TextStyle(
+              fontSize: 15.0,
+              color: Colors.black,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+              height:
+                  10.0),
+          Text(
+            "UNIVERSITAS ESA UNGGUL",
             style: TextStyle(
               fontSize: 20.0,
               color: Colors.black,
+              fontWeight: FontWeight.bold,
             ),
             textAlign: TextAlign.center,
           ),
@@ -81,4 +90,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
